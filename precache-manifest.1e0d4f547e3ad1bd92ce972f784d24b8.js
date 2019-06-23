b1f0fd17459cc8527140cd6978cdebd3f2b49cdf
Patryk <patrykkoski1994@gmail.com>
2019-06-23 21:59:56 +0200

    Updates

---
+++ precache-manifest.1e0d4f547e3ad1bd92ce972f784d24b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b94a5ed2d6e39817373d68319e2d4b",
+    "url": "/ice-cream2/index.html"
+  },
+  {
+    "revision": "f52f68cc3158e4cc289f",
+    "url": "/ice-cream2/static/css/main.26ce2c58.chunk.css"
+  },
+  {
+    "revision": "12f8be9be3a770aa94d7",
+    "url": "/ice-cream2/static/js/2.c4447f90.chunk.js"
+  },
+  {
+    "revision": "f52f68cc3158e4cc289f",
+    "url": "/ice-cream2/static/js/main.e3c91a97.chunk.js"
+  },
+  {
+    "revision": "5d25cfbf602438b4033b",
+    "url": "/ice-cream2/static/js/runtime~main.5436c30f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ice-cream2/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ice-cream2/precache-manifest.1e0d4f547e3ad1bd92ce972f784d24b8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ice-cream2/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
